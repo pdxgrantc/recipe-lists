ec8ad57950aa9f04846cd9f5892386994676cab1
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-04-04 11:53:41 -0700

    logos

--- src/Recipe/Recipe.js
+++ src/Recipe/Recipe.js
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Helmet } from 'react-helmet';
+
+// Firebase
+import { auth, db } from '../firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { doc, onSnapshot } from 'firebase/firestore'
+
+// Partials
+import Header from '../Static/Header/Header'
+import Footer from '../Static/Footer/Footer'
+import SignedOut from "../Static/SignedOut"
 
 export default function Recipe() {
-  return (
-    <div>Recipe</div>
-  )
+  const [user] = useAuthState(auth);
+
+  const [recipesData, setRecipesData] = useState({});
+
+  useEffect(() => {
+    const recipesDocRef = doc(db, "recipes", user.uid);
+    const unsubscribe = onSnapshot(recipesDocRef, (docSnapshot) => {
+      if (docSnapshot.exists()) {
+        setRecipesData(docSnapshot.data());
+      }
+    });
+
+    return () => unsubscribe();
+  }, [user.uid]);
+
+  if (!user) {
+    return (
+      <>
+        <div className="mx-auto bg-main_bg_color text-text_white min-h-[100vh] flex flex-col">
+          <Header />
+          <div className="w-full basis-auto grow">
+            <div className='mx-auto w-fit'>
+              <SignedOut />
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </>
+    )
+  }
+
+  else {
+    return (
+      <>
+        <Helmet>
+          <title>Recipe</title>
+        </Helmet>
+        <div className="bg-main_bg_color text-text_white h-[100vh] flex flex-col">
+          <Header />
+          <div className="w-full h-max basis-auto grow">
+            <div className='m-auto rounded-[10px] h-[80%] bg-black w-[90%]'>
+              <div className='flex gap-20 w-[100%] px-[4%] py-[3%]'>
+
+                {recipesData ? <>loaded</> : <>test</>}
+
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </>
+    )
+  }
 }
